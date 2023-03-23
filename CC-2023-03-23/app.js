ebd404905c9b5b5c1767f3a27e22917ae98ad772
Tessa Brunton <tesskbrunton@gmail.com>
2023-03-23 09:14:33 -0700

    Challenge and solution for 3-23-23 coding challenge

---
+++ CC-2023-03-23/app.js
@@ -0,0 +1,16 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+  let newArray = []
+  
+  array.forEach(num => num >= 0 ? newArray.push(-Math.abs(num)) : newArray.push(Math.abs(num)))
+  
+  return newArray
+  
+}
