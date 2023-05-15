eb8b94e50c3e49e0eb00c4e9cbe75fa46e3e5641
Tessa Brunton <tesskbrunton@gmail.com>
2023-05-14 19:30:14 -0700

    Challenge and solution for 5-14-23 coding challenge

---
+++ CC-2023-05-14/app.js
@@ -0,0 +1,18 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending) {
+  const length = ending.length;
+
+  if (ending.length === 0) {
+    return true;
+  } else if (str.slice(-length) === ending) {
+    return true;
+  } else {
+    return false;
+  }
+}
