52929ff43bc0ec670a481f01509317fc871d5065
Tessa Brunton <tesskbrunton@gmail.com>
2023-06-04 09:41:49 -0700

    Challenge and solution for 6-04-23 coding challenge

---
+++ CC-2023-06-04/app.js
@@ -0,0 +1,8 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds(values) {
+  const result = values.filter((num) => num % 2 === 0);
+  return result;
+}
