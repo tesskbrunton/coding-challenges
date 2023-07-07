b0bb4d0309e392ec3b7f9891a6cc7d3a5bf1289f
Tessa Brunton <tesskbrunton@gmail.com>
2023-07-07 08:20:25 -0700

    Challenge and solution for 7-07-23 coding challenge

---
+++ CC-2023-07-07/app.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Challenge:
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+// Example:
+
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+function flattenAndSort(array) {
+  const flattenArray = array.flat();
+  const sortArray = flattenArray.sort((a, b) => a - b);
+  return sortArray;
+}
