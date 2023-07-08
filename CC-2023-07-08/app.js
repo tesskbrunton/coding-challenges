f455387fa647b3d764fecdd42b7196362a440f03
Tessa Brunton <tesskbrunton@gmail.com>
2023-07-08 07:52:17 -0700

    Challenge and solution for 7-08-23 coding challenge

---
+++ CC-2023-07-08/app.js
@@ -0,0 +1,7 @@
+// Description:
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+function hexToDec(hexString) {
+  console.log(parseInt(hexString, 16));
+}
