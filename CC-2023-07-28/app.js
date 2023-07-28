956daffdda9b580671ed6bd8ef3eb35a63ffaa67
Tessa Brunton <tesskbrunton@gmail.com>
2023-07-28 08:18:48 -0700

    Challenge and solution for 7-28-23 coding challenge

---
+++ CC-2023-07-28/app.js
@@ -0,0 +1,30 @@
+// Description:
+
+// Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+// Note
+
+//     If num is negative, ignore its sign and treat it as a positive value
+//     If nth is not positive, return -1
+//     Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+
+// Examples(num, nth --> output)
+
+// 5673, 4 --> 5
+// 129, 2 --> 2
+// -2825, 3 --> 8
+// -456, 4 --> 0
+// 0, 20 --> 0
+// 65, 0 --> -1
+// 24, -8 --> -1
+
+var findDigit = function (num, nth) {
+  let string = String(Math.abs(num));
+
+  if (nth <= 0) {
+    return -1;
+  } else if (nth > string.length || num === 0) {
+    return 0;
+  } else {
+    return Number(string.charAt(string.length - nth));
+  }
+};
