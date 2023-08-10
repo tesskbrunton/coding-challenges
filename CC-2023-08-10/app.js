b02db6cc5218a32bee26650f8c69c1a794e136e2
Tessa Brunton <tesskbrunton@gmail.com>
2023-08-10 09:37:23 -0700

    Challenge and solution for 8-10-23 coding challenge

--- CC-2023-08-10/app.js
+++ CC-2023-08-10/app.js
@@ -1,8 +1,15 @@
-// Create a function that takes in an array of numbers
-// Alert the sum of the second lowest and the second highest number
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
-function sumSecondLowAndHigh(arr) {
-  const sortedArr = arr.slice().sort((a, b) => a - b);
-  const sum = sortedArr[1] + sortedArr[sortedArr.length - 2];
-  return sum;
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+  return dna.replace('T', 'U');
 }
