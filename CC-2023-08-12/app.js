2368beea667ae08b996fc70c6d3533eba4a4d424
Tessa Brunton <tesskbrunton@gmail.com>
2023-08-12 09:06:09 -0700

    Challenge and solution for 8-12-23 coding challenge

---
+++ CC-2023-08-12/app.js
@@ -0,0 +1,23 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+let number = function (array) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  let newArray = array.map((num, i) => {
+    return `${i + 1}: ${num}`;
+  });
+  return newArray;
+};
+
+console.log(number(["a", "b", "c"]))
