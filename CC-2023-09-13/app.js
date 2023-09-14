2d7fc72122b1667dfdb8d57b93d13a8be34ad921
Tessa Brunton <tesskbrunton@gmail.com>
2023-09-13 19:28:53 -0700

    Challenge and solution for 9-13-23 coding challenge

---
+++ CC-2023-09-13/app.js
@@ -0,0 +1,18 @@
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+// Examples
+
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(s) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  const array = s.split('');
+  array.map((letter, i) => {
+    if (vowels.includes(letter)) {
+      return (array[i] = '!');
+    }
+  });
+  return array.join('');
+}
