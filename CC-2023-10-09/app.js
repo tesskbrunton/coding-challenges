87a2417478d3e6df4b53a3dac1d232b6d023bb5d
Tessa Brunton <tesskbrunton@gmail.com>
2023-10-09 08:16:04 -0700

    Challenge and solution for 10-9-23 coding challenge

---
+++ CC-2023-10-09/app.js
@@ -0,0 +1,17 @@
+// Description:
+
+// In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+
+// Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+
+// Good Luck!
+// Note
+
+// 1 Dollar = 100 Cents
+
+function fuelPrice(litres, pricePerLitre) {
+  const totalDiscount = ((Math.floor(litres / 2) * 2) / 2) * 0.05;
+  const discount = totalDiscount < 0.25 ? totalDiscount : 0.25;
+  const totalPrice = (pricePerLitre - discount) * litres;
+  return Number(totalPrice.toFixed(2));
+}
