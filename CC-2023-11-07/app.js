0ed903e86ce90b219cb0712e80f47d62505c2c38
Tessa Brunton <tesskbrunton@gmail.com>
2023-11-07 11:58:24 -0800

    Challenge and solution for 11-7-23 coding challenge

---
+++ CC-2023-11-07/app.js
@@ -0,0 +1,37 @@
+// Description:
+
+// Given an unsorted array of 3 positive integers [ n1, n2, n3 ], determine if it is possible to form a Pythagorean Triple using those 3 integers.
+
+// A Pythagorean Triple consists of arranging 3 integers, such that:
+
+// a2 + b2 = c2
+// Examples
+
+// [5, 3, 4] : it is possible to form a Pythagorean Triple using these 3 integers: 32 + 42 = 52
+
+// [3, 4, 5] : it is possible to form a Pythagorean Triple using these 3 integers: 32 + 42 = 52
+
+// [13, 12, 5] : it is possible to form a Pythagorean Triple using these 3 integers: 52 + 122 = 132
+
+// [100, 3, 999] : it is NOT possible to form a Pythagorean Triple using these 3 integers - no matter how you arrange them, you will never find a way to satisfy the equation a2 + b2 = c2
+// Return Values
+
+//     For Python: return True or False
+//     For JavaScript: return true or false
+//     Other languages: return 1 or 0 or refer to Sample Tests.
+
+function isPythagoreanTriple(integers) {
+  let num1 = integers[0] ** 2;
+  let num2 = integers[1] ** 2;
+  let num3 = integers[2] ** 2;
+
+  if (num1 + num2 === num3) {
+    return true;
+  } else if (num2 + num3 === num1) {
+    return true;
+  } else if (num1 + num3 === num2) {
+    return true;
+  } else {
+    return false;
+  }
+}
